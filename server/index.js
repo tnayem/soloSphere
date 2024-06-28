44142bdf9d7ca6ea2cf04c3de15a3811f4f4df0a
Md Tasdid Nayem <tasdidnayem@gmail.com>
2024-06-29 02:24:53 +0600

    get data from server

--- server/index.js
+++ server/index.js
@@ -14,9 +14,43 @@ const corsOptions = {
 }
 app.use(cors(corsOptions))
 app.use(express.json())
+// soloSphere
+// LAhD2lcmfxLefus7
 
-app.get('/', async(req,res)=>{
-    console.log("This server is Ok for everything");
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ycg9h6w.mongodb.net/?appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        const jobsCollection = client.db('soloSphere').collection('jobs')
+        const bidsCollection = client.db('soloSphere').collection('bids')
+        // Get All Jobs Data from db
+        app.get('/jobs', async(req,res)=>{
+            const result = await jobsCollection.find().toArray()
+            res.send(result)
+        })
+        
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+app.get('/', async (req, res) => {
+    res.send("This server is Ok for everything");
 })
 
 
